922b51290e6e5799716a7c0bcec660f106d7b4ef
WHITEROSE <WHITEROSE@gli-pc>
2017-01-14 09:22:49 -0600

    Cell and Grid work mostly finished.

--- gliGOL/src/gliGOL/Cell.java
+++ gliGOL/src/gliGOL/Cell.java
@@ -1,14 +1,73 @@
 package gliGOL;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public class Cell {
 	private int x;
 	private int y;
+	private int cellSize;
 	private boolean isAlive;
+	private Cell left, right, up, down, upleft, upright, downleft, downright;
+	private Color color;
 
-	public Cell(int x, int y, boolean isAlive) {
+	public Cell(int x, int y, int cellSize, boolean isAlive) {
 		this.setX(x);
 		this.setY(y);
 		this.setAlive(isAlive);
+		this.setCellSize(cellSize);
+		this.setColor(Color.black);
+		initNeighbors();
+	}
+
+	private void setCellSize(int cellSize) {
+		this.cellSize = cellSize;
+	}
+
+	public void initNeighbors() {
+		setNeighbors(null, null, null, null, null, null, null, null);
+	}
+
+	public void setNeighbors(Cell left, Cell right, Cell up, Cell down, Cell upleft, Cell upright, Cell downleft,
+			Cell downright) {
+		setLeft(left);
+		setRight(right);
+		setUp(up);
+		setDown(down);
+		setUpleft(upleft);
+		setUpright(upright);
+		setDownleft(downleft);
+		setDownright(downright);
+	}
+
+	public int getNumberOfAliveNeighbors() {
+		int aliveCount = 0;
+		if (null != left && left.isAlive()) {
+			aliveCount++;
+		}
+		if (null != right && right.isAlive()) {
+			aliveCount++;
+		}
+		if (null != up && up.isAlive()) {
+			aliveCount++;
+		}
+		if (null != down && down.isAlive()) {
+			aliveCount++;
+		}
+		if (null != upleft && upleft.isAlive()) {
+			aliveCount++;
+		}
+		if (null != upright && upright.isAlive()) {
+			aliveCount++;
+		}
+		if (null != downleft && downleft.isAlive()) {
+			aliveCount++;
+		}
+		if (null != downright && downright.isAlive()) {
+			aliveCount++;
+		}
+		return aliveCount;
 	}
 
 	public int getX() {
@@ -33,5 +92,90 @@ public class Cell {
 
 	public void setAlive(boolean isAlive) {
 		this.isAlive = isAlive;
+	}
+
+	public Cell getLeft() {
+		return left;
+	}
+
+	public void setLeft(Cell left) {
+		this.left = left;
+	}
+
+	public Cell getRight() {
+		return right;
+	}
+
+	public Cell setRight(Cell right) {
+		this.right = right;
+		return right;
+	}
+
+	public Cell getUp() {
+		return up;
+	}
+
+	public Cell setUp(Cell up) {
+		this.up = up;
+		return up;
+	}
+
+	public Cell getDown() {
+		return down;
+	}
+
+	public Cell setDown(Cell down) {
+		this.down = down;
+		return down;
+	}
+
+	public Cell getUpleft() {
+		return upleft;
+	}
+
+	public Cell setUpleft(Cell upleft) {
+		this.upleft = upleft;
+		return upleft;
+	}
+
+	public Cell getUpright() {
+		return upright;
+	}
+
+	public Cell setUpright(Cell upright) {
+		this.upright = upright;
+		return upright;
+	}
+
+	public Cell getDownleft() {
+		return downleft;
+	}
+
+	public Cell setDownleft(Cell downleft) {
+		this.downleft = downleft;
+		return downleft;
+	}
+
+	public Cell getDownright() {
+		return downright;
+	}
+
+	public Cell setDownright(Cell downright) {
+		this.downright = downright;
+		return downright;
+	}
+
+	public void draw(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(this.getColor());
+		g2.drawRect((x / cellSize) * cellSize, ((y / cellSize) * cellSize), cellSize, cellSize);
+	}
+
+	private Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color c) {
+		this.color = c;
 	}
 }
--- gliGOL/src/gliGOL/Grid.java
+++ gliGOL/src/gliGOL/Grid.java
@@ -1,5 +1,81 @@
 package gliGOL;
 
-public class Grid {
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+public class Grid {
+	private Cell grid[][];
+	private int gridWidth;
+	private int gridHeight;
+	private int cellSize;
+
+	public Grid() {
+		setGridWidth(10);
+		setGridHeight(10);
+		setCellSize(8);
+		grid = new Cell[getGridHeight()][getGridWidth()];
+		for (int x = 0; x < getGridWidth(); x++) {
+			for (int y = 0; y < getGridHeight(); y++) {
+				grid[y][x] = new Cell(x, y, cellSize, false);
+			}
+		}
+		for (int x = 0; x < getGridWidth(); x++) {
+			for (int y = 0; y < getGridHeight(); y++) {
+				Cell currentCell = grid[y][x];
+				if ((x - 1) > 0) {
+					currentCell.setLeft(grid[y][x - 1]);
+				}
+				if ((x + 1) < getGridWidth()) {
+					currentCell.setRight(grid[y][x + 1]);
+				}
+				if ((y - 1) > 0) {
+					currentCell.setUp(grid[y - 1][x]);
+				}
+				if ((y + 1) < getGridHeight()) {
+					currentCell.setDown(grid[y + 1][x]);
+				}
+				if (((x - 1) > 0) && ((y - 1) > 0)) {
+					currentCell.setUpleft(grid[y - 1][x - 1]);
+				}
+				if (((x + 1) < getGridWidth()) && ((y - 1) > 0)) {
+					currentCell.setUpright(grid[y - 1][x + 1]);
+				}
+				if (((x - 1) > 0) && ((y + 1) < getGridHeight())) {
+					currentCell.setDownleft(grid[y + 1][x - 1]);
+				}
+				if (((x + 1) < getGridWidth()) && ((y + 1) < getGridHeight())) {
+					currentCell.setDownright(grid[y + 1][x + 1]);
+				}
+			}
+		}
+	}
+
+	public int getGridWidth() {
+		return gridWidth;
+	}
+
+	public void setGridWidth(int gridWidth) {
+		this.gridWidth = gridWidth;
+	}
+
+	public int getGridHeight() {
+		return gridHeight;
+	}
+
+	public void setGridHeight(int gridHeight) {
+		this.gridHeight = gridHeight;
+	}
+
+	public int getCellSize() {
+		return cellSize;
+	}
+
+	public void setCellSize(int cellSize) {
+		this.cellSize = cellSize;
+	}
+
+	public void draw(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+
+	}
 }
